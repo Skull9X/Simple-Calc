49c4a34b49fb349d874e12a56b5044730117c43b
ABDO <120413124+Skull9X@users.noreply.github.com>
2022-12-16 17:50:54 +0200

    Add files via upload

---
+++ calc_V2_update.py
@@ -0,0 +1,328 @@
+import os , time
+os.system('clear') 
+from time import sleep as timeout
+from Style import skull
+
+# ×××××××××××××××××××××××××××××
+
+#droid = androidhelper.Android()
+#droid.makeToast("🔒You Are Hacked ☠️🗡️")
+#def python_speak():
+#    test = androidhelper.Android()
+#    result = test.ttsSpeak('Hello')
+#    return result.error is None
+#python_speak()
+
+# ××××××××××××××××××××××××××××
+
+def add(x , y):
+    return x + y
+def sub(x , y):
+    return x - y
+def multiply(x , y):
+    return x * y
+def divide(x , y):
+    return x / y
+def reminder(x , y):
+    return x % y
+def power(x , y):
+    return x ** y
+def Max_Number(x , y):
+    return max(x , y)
+def Square(x):
+    return x ** 2
+def sqrt(x):
+    from math import sqrt    
+    return sqrt(x)
+def cube(x):
+    return x*x*x
+def Average(n,sum):
+    return sum / n
+def FloorDivision(x , y):
+    return x // y
+def TrigonometricFunctions() :
+    
+    from math import sin,cos,tan,sinh,cosh,tanh,asinh,acosh,atanh,asin,acos,atan
+    y = 'Y'
+    while y == 'Y' :
+        x = float(input("\t Enter The Seta : "))
+        z = input("\t Choice The Trigonometric function\n\t [sin,cos,tan,\n\tsinh,cosh,tanh\n\t,asinh,acosh,atanh,\n\tasin,acos,atan] : ")
+        print("\n")
+        if z == 'sin' :
+            print(f"  The Sin of {x} = ",sin(x),"\n")
+        elif z == 'cos' :
+            print(f"  The Cos of {x} = ",cos(x),"\n")
+        elif z == 'tan' :
+            print(f"  The Tan of {x} = ",tan(x),"\n")
+        elif z == 'sinh' :
+            print(f"  The Sinh of {x} = ",sinh(x),"\n")
+        elif z == 'cosh' :
+            print(f"  The Cosh of {x} = ",cosh(x),"\n")
+        elif z == 'tanh' :
+            print(f"  The Tanh of {x} = ",tanh(x),"\n")
+        elif z == 'asinh' :
+            print(f"  The aSinh of {x} = ",asinh(x),"\n")
+        elif z == 'acosh' :
+            print(f"  The aCosh of {x} = ",acosh(x),"\n")
+        elif z == 'atanh' :
+            print(f"  The aTanh of {x} = ",atanh(x),"\n")
+        elif z == 'asin' :
+            print(f"  The aSin of {x} = ",asin(x),"\n")
+        elif z == 'acos' :
+            print(f"  The aCos of {x} = ",acos(x),"\n")
+        elif z == 'atan' :
+            print(f"  The aTan of {x} = ",atan(x),"\n")
+        else :
+            print("\t Value Error ")
+
+        y = input("\t Continue [Y/N] : ").upper()
+        print("\n")
+
+
+
+
+# ×××××××××××××××××××××××××××××
+
+def Poison():
+
+    time.sleep(0.1)
+    print("\033[1;92m   _____   _____   _   _____   _____   __   _ ")
+    time.sleep(0.1)
+    print("\033[1;92m  |  _  \ /  _  \ | | /  ___/ /  _  \ |  \ | |")
+    time.sleep(0.1)
+    print("\033[1;92m  | |_| | | | | | | | | |___  | | | | |   \| |")
+    time.sleep(0.1)
+    print("\033[1;92m  |  ___/ | | | | | | \___  \ | | | | | |\   |")
+    time.sleep(0.1)
+    print("\033[1;92m  | |     | |_| | | |  ___| | | |_| | | | \  |")
+    time.sleep(0.1)
+    print("\033[1;92m  |_|     \_____/ |_| /_____/ \_____/ |_|  \_|\n\n")
+    time.sleep(0.3)
+
+
+def Bn1():
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ..!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ...!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ..!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ...!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ..!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ...!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ..!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ...!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ..!')
+    time.sleep(0.2)
+    os.system('clear')
+    print(' \033[1;93mL \033[1;31mO \033[1;34mA \033[1;32mD \033[1;34mI \033[1;32mN \033[1;93mG ...!')
+    time.sleep(0.2)
+    os.system('clear')
+Bn1()
+    
+    
+def Bn2():
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...- ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT .../ ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...- ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...\ ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...- ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT .../ ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...- ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...\ ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT ...- ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print (' \033[1;93mP \033[1;31m L  \033[1;34mE  \033[1;32mA  \033[1;34mS \033[1;32m E    \033[1;93mW   \033[1;32mA  \033[1;34mI  \033[1;93mT .../ ')
+    time.sleep(0.2)
+    os.system ('clear')
+    print ("\033[92m\n\t`YOU ARE HACKED`")
+    time.sleep(3)
+    os.system ('clear')
+    
+Bn2()
+Poison()
+skull()
+
+time.sleep(0.1)
+print("\t\t🇻 🇮 🇷 🇺 🇸    💀       ")
+time.sleep(0.1)
+print("  ===========================================\n")
+time.sleep(0.1)
+
+# ×××××××××××××××××××××××××××××
+
+print("  \033[1;91m[1] Add")
+time.sleep(0.1)
+print("  \033[1;92m[2] Sub")
+time.sleep(0.1)
+print("  \033[1;93m[3] Multiply")
+time.sleep(0.1)
+print("  \033[1;94m[4] Divide")
+time.sleep(0.1)
+print("  \033[1;95m[5] Reminder")
+time.sleep(0.1)
+print("  \033[1;96m[6] Power")
+time.sleep(0.1)
+print("  \033[1;91m[7] Max Number")
+time.sleep(0.1)
+print("  \033[1;92m[8] Square")
+time.sleep(0.1)
+print("  \033[1;93m[9] Sqrt")
+time.sleep(0.1)
+print("  \033[1;94m[10] Cube")
+time.sleep(0.1)
+print("  \033[1;95m[11] Average")
+time.sleep(0.1)
+print("  \033[1;96m[12] Floor Division")
+time.sleep(0.1)
+print("  \033[1;91m[13] Trigonometric Functions\n")
+time.sleep(0.1)
+print("\033[1;91m-" * 46)
+time.sleep(0.1)
+print("  \033[1;92mchoice1(c1)\033[1;93m[1:7] \033[1;92m| choice2(c2)\033[1;93m[8:13]")
+print("\033[1;91m-" * 46)
+time.sleep(0.1)
+print("  \033[1;92mEnter\033[1;91m c1 or c2 \033[1;92mto start \n  And choose from \033[1;91m1 : 13 \n  \033[1;92mto turn on calculater")
+time.sleep(0.1)
+print("\033[1;91m-" * 46)
+time.sleep(0.1)
+print('  Press [99] To Exit ...!')
+print("  \033[1;92m===========================================")
+time.sleep(0.1)
+
+
+
+l = 'Y'
+while l == 'Y' :
+    
+    choiceT = input("\n\033[0;95m  $ ~ [choice1(c1)] | [choice2(c2)]: ")
+    
+#    Aaa = 'Y'
+#    while True:
+    if choiceT == 'choice1' or choiceT == 'c1' :        
+        choice1 = input("\n\t\033[0;95mEnter Choice (1/2/3/4/5/6/7) : ")
+        print("\n")
+        if choice1 in ('1','2','3','4','5','6','7') :
+            
+        
+            num1 = float(input("\t\033[0;96mEnter First Number : "))
+            num2 = float(input("\t\033[0;94mEnter Second Number : "))
+        
+            if choice1 == '1':
+                print("\t\033[0;91m",num1 ,"+", num2, "=",add(num1 , num2))
+    
+            elif choice1 == '2':
+                print("\t\033[0;92m",num1 ,"-", num2, "=",sub(num1 , num2))
+                
+            elif choice1 == '3':
+                print("\t\033[0;93m",num1 ,"*", num2, "=",multiply(num1 , num2))
+        
+            elif choice1 == '4':
+                print("\t\033[0;94m",num1 ,"/", num2, "=",divide(num1 , num2))
+            
+            elif choice1 == '5':
+                print("\t\033[0;95m",num1 ,"%", num2, "=",reminder(num1 , num2))
+                
+            elif choice1 == '6':
+                print("\t\033[0;96m",num1 ,"**", num2, "=",power(num1 , num2))
+                
+            elif choice1 == '7':
+                if num1 == num2 :
+                    print ("\033[0;92m\n\tThe First Number\n\tAnd Second Number Are Equal")
+                else :
+                    print("\t\033[0;92m","The \033[0;91m[Max] \033[0;92mNumber is:",Max_Number(num1 , num2))
+                    
+        else :
+        	print('error')
+        
+                
+             
+    elif choiceT == 'choice2' or choiceT == 'c2' :
+        choice2 = input("\n\t\033[0;95mEnter Choice (8/9/10/11/12/13) : ")
+        print("\n")
+        if choice2 in ('8','9','10','11','12','13') :
+            
+
+
+            if choice2 == '8':
+                xo = float(input("  \033[0;96mEnter a Number you Want to Square : "))
+                Square1 = Square(xo)
+                print("\t\033[0;93mThe Square of {0} = {1}".format(xo,Square1))
+            
+            elif choice2 == '9':
+                nm = float(input("  \033[0;96mEnter a Number You Want to Sqrt : "))
+                sqrt1 = sqrt(nm)
+                print("\t\033[0;91m","The Sqrt of {0} = {1}".format(nm,sqrt1))
+            
+            elif choice2 == '10':
+                num = float(input("  \033[0;96mEnter a Number You Want to Cube : "))
+                cube1 = cube(num)
+                print("\t\033[0;91m","The Cube of {0} = {1}".format(num,cube1))
+        
+            elif choice2 == '11':
+                x = 0
+                sum = 0.0
+                num = 1
+                n = int(input("  \t\033[0;91mEnter Sum of Numbers : "))
+                for i in range(n):
+                    i += 1 
+                    num = float(input("  \t\033[0;96mEnter Number {} : ".format(i)))
+                    sum = sum + num
+                    x += 1
+                print("\t\033[1;92mAverage : ",sum / n)
+                    
+                
+            elif choice2 == '12':
+               f1 = float(input("\t\033[0;96mEnter First Number : "))
+               f2 = float(input("\t\033[0;94mEnter Second Number : "))
+               
+               print("\t\033[0;91m",f1 ,"//", f2, "=", FloorDivision(f1 , f2))
+                   
+            elif choice2 == '13':
+                TrigonometricFunctions()
+        else :
+               print('errorrr')
+                          
+    elif choiceT == '99':
+        break
+        os.system('clear')
+        	
+    else :
+        print("\n\t\033[0;93mInvalid Input")
+                                
+    print("\n")          
+    l = input ("\t\033[1;95mDo You Want To Continue[Y/N] : ").upper()
+    
+
